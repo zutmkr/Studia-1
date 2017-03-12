0929c7063157a1c303f363ffd63e7d60e821a0f5
ananeke <ananeke@gmail.com>
2017-03-12 16:02:04 +0100

    Usprawnienie kodu

--- OOP_python_laborki/LAB 3/Zadanie_2_Pokoje.py
+++ OOP_python_laborki/LAB 3/Zadanie_2_Pokoje.py
@@ -1,30 +1,34 @@
-from msvcrt import getch
 from time import sleep
 import random
 import os
 
-stuff = ['miecz', 'zbroja', 'potion', 'buty', 'kusza', 'tiara', 'pierscien', 'amulet', 'tarcza', 'pas', 'rekawice', 'pet']
+stuff = ['miecz', 'zbroja', 'potion', 'buty', 'kusza', 'tiara', 'pierscien', 'amulet', 'tarcza', 'pas', 'rekawice',
+         'pet']
 dobry_prefix = ['dobre', 'magiczne', 'legendarne', 'rzadkie']
 zly_prefix = ['znoszone', 'przeklete', 'zepsute', 'nawiedzone']
-rodzaj_mapy = [[0,0,0],[0,0,0,0,0],[0,0,0,0,0,0,0]]
-prawda_falsz = [True,False]
-class Przedmiot():
+rodzaj_mapy = [[0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+prawda_falsz = [True, False]
+
+
+class Przedmiot:
     def __init__(self):
         self.nazwa = random.choice(stuff)
-class Pokoj():
+
+
+class Pokoj:
     otwarty = True
     loot = Przedmiot()
     droga = False
 
-class Mapa():
 
+class Mapa():
     def __init__(self):
         # tworzenie mapy gry
         wielkosc_mapy = random.choice(rodzaj_mapy)
         self.mapa = []
         for i in range(len(wielkosc_mapy)): self.mapa.append(wielkosc_mapy[:])
         print(len(wielkosc_mapy))
-        self.lista_pokoi = [Pokoj() for i in range(len(wielkosc_mapy)**2)]
+        self.lista_pokoi = [Pokoj() for i in range(len(wielkosc_mapy) ** 2)]
         k = 0
         for i in range(len(wielkosc_mapy)):
             for j in range(len(wielkosc_mapy)):
@@ -33,7 +37,7 @@ class Mapa():
                     self.mapa[i][j].otwarty = True
                 else:
                     self.mapa[i][j].otwarty = False
-                k +=1
+                k += 1
 
     def PrzygotujMape(self):
         if len(self.mapa[0]) == 3:
@@ -68,16 +72,14 @@ class Mapa():
             self.mapa[6][5].otwarty = True
             self.mapa[6][6].otwarty = True
 
-
-
     def RysujMape(self):
-        os.system('cls')  # czyszczenie działa na windowsowej konsoli
+        os.system('cls')  # czyszczenie ekranu
         LOGO()
-        for i in range (len(self.mapa[0])):
+        for i in range(len(self.mapa[0])):
             for j in range(len(self.mapa[0])):
                 try:
                     if self.mapa[i][j].otwarty:
-                        if j == len(self.mapa[0])-1:
+                        if j == len(self.mapa[0]) - 1:
                             print('_')
                         else:
                             print('_', end='')
@@ -93,7 +95,6 @@ class Mapa():
                         print('8', end='')
 
 
-
 class Wartosciowy(Przedmiot):
     def dodaj_przedmiot(self):
         z = random.choice(dobry_prefix)
@@ -101,6 +102,7 @@ class Wartosciowy(Przedmiot):
         z = z + ' ' + y.nazwa
         y.nazwa = z
         return y
+
 
 class Smiec(Przedmiot):
     def dodaj_przedmiot(self):
@@ -110,10 +112,12 @@ class Smiec(Przedmiot):
         y.nazwa = z
         return y
 
+
 class Gracz():
     lista = []
     punkty = 0
     pozycja = []
+
     def pokaz_plecak(self):
         for Przedmiot in self.lista:
             print(Przedmiot.nazwa)
@@ -121,92 +125,90 @@ class Gracz():
     def pobierzPozycje(self, Maps):
         for i in range(len(Maps.mapa)):
             try:
-                if (Maps.mapa[i].index(self)>= 0):
+                if (Maps.mapa[i].index(self) >= 0):
                     self.pozycja = []
                     self.pozycja.append(i)
                     self.pozycja.append(Maps.mapa[i].index(self))
             except:
                 continue
 
+    def Dodaj_do_Plecak(self):
+        if random.choice(prawda_falsz):
+            self.lista.append(Wartosciowy().dodaj_przedmiot())
+            self.punkty += 1
+        else:
+            self.lista.append(Smiec().dodaj_przedmiot())
+            self.punkty -= 1
 
-def PoruszaniePoMapie(gr,Maps):
+
+def PoruszaniePoMapie(gr, Maps):
     gr.pobierzPozycje(Maps)
+
+    def Sciana(t):
+        print('Nie mozesz tam isc, to sciana.')
+        sleep(t)
+
+    def Stala_pozycja(gr, Maps):
+        Maps.mapa[gr.pozycja[0]][gr.pozycja[1]] = Pokoj()
+        gr.pobierzPozycje(Maps)
+        gr.Dodaj_do_Plecak()
+
+    def W(gr, Maps):
+        if gr.pozycja[0] - 1 >= 0 and Maps.mapa[gr.pozycja[0] - 1][gr.pozycja[1]].otwarty:
+            Maps.mapa[gr.pozycja[0] - 1][gr.pozycja[1]] = gr
+            Stala_pozycja(gr, Maps)
+        else:
+            Sciana(2)
+
+    def S(gr, Maps):
+        if gr.pozycja[0] + 1 < len(Maps.mapa) and Maps.mapa[gr.pozycja[0] + 1][gr.pozycja[1]].otwarty:
+            Maps.mapa[gr.pozycja[0] + 1][gr.pozycja[1]] = gr
+        else:
+            Sciana(2)
+
+    def A(gr, Maps):
+        if gr.pozycja[1] - 1 >= 0 and Maps.mapa[gr.pozycja[0]][gr.pozycja[1] - 1].otwarty:
+            Maps.mapa[gr.pozycja[0]][gr.pozycja[1] - 1] = gr
+            Stala_pozycja(gr, Maps)
+        else:
+            Sciana(2)
+
+    def D(gr, Maps):
+        if gr.pozycja[1] + 1 < len(Maps.mapa) and Maps.mapa[gr.pozycja[0]][gr.pozycja[1] + 1].otwarty:
+            Maps.mapa[gr.pozycja[0]][gr.pozycja[1] + 1] = gr
+        else:
+            Sciana(2)
+
     while True:
-		print('\n\nQuo vadis?>', end='')
-        h = ord(getch())
-        if h == 119:
-            if gr.pozycja[0] - 1 >= 0 and Maps.mapa[gr.pozycja[0]-1][gr.pozycja[1]].otwarty == True:
-                Maps.mapa[gr.pozycja[0] - 1][gr.pozycja[1]] = gr
-                Maps.mapa[gr.pozycja[0]][gr.pozycja[1]] = Pokoj()
-                gr.pobierzPozycje(Maps)
-                #print(gr.pozycja[0], gr.pozycja[1])
-                if random.choice(prawda_falsz):
-                    gr.lista.append(Wartosciowy().dodaj_przedmiot())
-                    gr.punkty +=1
-                else:
-                    gr.lista.append(Smiec().dodaj_przedmiot())
-                    gr.punkty -=1
+        print('\n\nQuo vadis?>', end='')
+        h = input()
+        if h == 'w':
+            W(gr, Maps)
+        elif h == 's':
+            S(gr, Maps)
+            if gr.pozycja[0] == len(Maps.mapa) - 1 and gr.pozycja[1] == len(Maps.mapa) - 1:
+                print('Wygrales, oto Twoje skarby: ')
+                gr.pokaz_plecak()
+                print('Zdobyles :', gr.punkty, 'punktow')
+                exit(0)
             else:
-                print('Nie mozesz tam isc, to sciana.')
-        elif h == 115:
-            if gr.pozycja[0] + 1 < len(Maps.mapa) and Maps.mapa[gr.pozycja[0]+1][gr.pozycja[1]].otwarty == True:
-                Maps.mapa[gr.pozycja[0] + 1][gr.pozycja[1]] = gr
-                Maps.mapa[gr.pozycja[0]][gr.pozycja[1]] = Pokoj()
-                gr.pobierzPozycje(Maps)
-                #print(gr.pozycja[0], gr.pozycja[1])
-                if gr.pozycja[0] == len(Maps.mapa)-1 and gr.pozycja[1] == len(Maps.mapa)-1:
-                    print('Wygrales, oto Twoje skarby: ')
-                    gr.pokaz_plecak()
-                    print('Zdobyles :', gr.punkty, 'punktow')
-                    exit(0)
-                else:
-                    if random.choice(prawda_falsz):
-                        gr.lista.append(Wartosciowy().dodaj_przedmiot())
-                        gr.punkty +=1
-                    else:
-                        gr.lista.append(Smiec().dodaj_przedmiot())
-                        gr.punkty -=1
+                Stala_pozycja(gr, Maps)
+        elif h == 'a':
+            A(gr, Maps)
+        elif h == 'd':
+            D(gr, Maps)
+            if gr.pozycja[0] == len(Maps.mapa) - 1 and gr.pozycja[1] == len(Maps.mapa) - 1:
+                print('Wygrales, oto Twoje skarby: ')
+                gr.pokaz_plecak()
+                print('Zdobyles :', gr.punkty, 'punktow')
+                exit(0)
             else:
-                print('Nie mozesz tam isc, to sciana.')
-        elif h == 97:
-            if gr.pozycja[1] - 1 >= 0 and Maps.mapa[gr.pozycja[0]][gr.pozycja[1]-1].otwarty == True:
-                Maps.mapa[gr.pozycja[0]][gr.pozycja[1] - 1] = gr
-                Maps.mapa[gr.pozycja[0]][gr.pozycja[1]] = Pokoj()
-                gr.pobierzPozycje(Maps)
-                #print(gr.pozycja[0], gr.pozycja[1])
-                if random.choice(prawda_falsz):
-                    gr.lista.append(Wartosciowy().dodaj_przedmiot())
-                    gr.punkty +=1
-                else:
-                    gr.lista.append(Smiec().dodaj_przedmiot())
-                    gr.punkty -=1
-            else:
-                print('Nie mozesz tam isc, to sciana.')
-        elif h == 100:
-            if gr.pozycja[1] + 1 < len(Maps.mapa) and Maps.mapa[gr.pozycja[0]][gr.pozycja[1]+1].otwarty == True:
-                Maps.mapa[gr.pozycja[0]][gr.pozycja[1] + 1] = gr
-                Maps.mapa[gr.pozycja[0]][gr.pozycja[1]] = Pokoj()
-                gr.pobierzPozycje(Maps)
-                #print(gr.pozycja[0], gr.pozycja[1])
-                if gr.pozycja[0] == len(Maps.mapa)-1 and gr.pozycja[1] == len(Maps.mapa)-1:
-                    print('Wygrales, oto Twoje skarby: ')
-                    gr.pokaz_plecak()
-                    print('Zdobyles :', gr.punkty, 'punktow')
-                    exit(0)
-                else:
-                    if random.choice(prawda_falsz):
-                        gr.lista.append(Wartosciowy().dodaj_przedmiot())
-                        gr.punkty +=1
-                    else:
-                        gr.lista.append(Smiec().dodaj_przedmiot())
-                        gr.punkty -=1
-            else:
-                print('Nie mozesz tam isc, to sciana.')
+                Stala_pozycja(gr, Maps)
         else:
             print('Zla droga kroczysz!')
-            
-				sleep(1)
+
         Maps.RysujMape()
+
 
 def LOGO():
     with open('LOGO.txt') as plik:
@@ -223,4 +225,4 @@ Maps.PrzygotujMape()
 
 Maps.RysujMape()
 
-PoruszaniePoMapie(gr,Maps)
+PoruszaniePoMapie(gr, Maps)
